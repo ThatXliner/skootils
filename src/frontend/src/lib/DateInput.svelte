a8c68e41af9038ac0b22d9fd9776e29527319d74
Bryan Hu <bryan.hu.2020@gmail.com>
2022-06-10 22:35:14 -0700

    :recycle: Minor changes in frontend

--- src/frontend/src/lib/DateInput.svelte
+++ src/frontend/src/lib/DateInput.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
-
-	const dispatch = createEventDispatcher();
 	const MONTHS = [
 		'January',
 		'February',
@@ -16,72 +13,111 @@
 		'November',
 		'December'
 	];
-	// let _selected: string = new Date().toISOString().slice(0, 10);
 	let selectedMonth: string | null = null;
 	let selectedDay: number = 1;
+	$: selected = selectedMonth !== null ? `${selectedMonth} ${selectedDay}` : 'today';
+	export let selectedDates: string[] = [];
 	function select() {
-		dispatch('select', `${selectedMonth} ${selectedDay}`);
+		selectedDates = [selected, ...selectedDates];
 		selectedMonth = null;
 		selectedDay = 1;
 	}
 </script>
 
-<div class="flex space-x-2 my-2">
-	<div class="dropdown">
-		{#if selectedMonth == null}
-			<label for="month-select" tabindex="0" class="btn">Select a month</label>
-		{:else}
-			<label for="month-select" tabindex="0" class="btn btn-accent">{selectedMonth}</label>
-		{/if}
-		<ul
-			id="month-select"
-			tabindex="0"
-			class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 max-h-52 overflow-y-scroll"
+<div>
+	<div class="flex space-x-2 my-2">
+		<div class="dropdown">
+			{#if selectedMonth == null}
+				<label for="month-select" tabindex="0" class="btn">Select a month</label>
+			{:else}
+				<label for="month-select" tabindex="0" class="btn btn-accent">{selectedMonth}</label>
+			{/if}
+			<ul
+				id="month-select"
+				tabindex="0"
+				class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 max-h-52 overflow-y-scroll"
+			>
+				{#each MONTHS as month}
+					<li
+						class:active={month == selectedMonth}
+						on:click={() => {
+							selectedMonth = month;
+						}}
+					>
+						<span>{month}</span>
+					</li>
+				{/each}
+			</ul>
+		</div>
+		<div class="btn-group">
+			<button
+				class="btn text-lg"
+				on:click={() => {
+					selectedDay--;
+				}}>-</button
+			>
+			<input
+				type="number"
+				class="w-10 text-center"
+				bind:value={selectedDay}
+				on:blur={() => {
+					if (selectedDay < 1) {
+						selectedDay = 1;
+					}
+					if (selectedDay > 31) {
+						selectedDay = 31;
+					}
+				}}
+			/>
+
+			<button
+				class="btn text-lg"
+				on:click={() => {
+					selectedDay++;
+				}}>+</button
+			>
+		</div>
+
+		<button
+			on:click={select}
+			class="btn btn-primary"
+			class:btn-disabled={selectedMonth == null || selectedDates.includes(selected)}>Add</button
 		>
-			{#each MONTHS as month}
+	</div>
+
+	{#if selectedDates.length > 0}
+		<ul id="date-list" class="flex flex-col p-2 border-2 rounded-box w-fit">
+			{#each selectedDates as date, i}
+				<!-- TODO: argh where do we put the trash can icon -->
 				<li
-					class:active={month == selectedMonth}
+					class="my-1 btn btn-accent"
 					on:click={() => {
-						selectedMonth = month;
+						selectedDates = selectedDates.slice(0, i).concat(selectedDates.slice(i + 1));
 					}}
 				>
-					<span>{month}</span>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<span class="mx-1">{date}</span>
 				</li>
 			{/each}
 		</ul>
-	</div>
-	<div class="btn-group">
-		<button
-			class="btn text-lg"
-			on:click={() => {
-				selectedDay--;
-			}}>-</button
+	{:else}
+		<p
+			class="text-red-500 text-center font-bold border-2 dark:bg-neutral p-2 py-5 rounded-box w-fit"
 		>
-		<input
-			type="number"
-			class="w-10 p-2"
-			bind:value={selectedDay}
-			on:blur={() => {
-				if (selectedDay < 1) {
-					selectedDay = 1;
-				}
-				if (selectedDay > 31) {
-					selectedDay = 31;
-				}
-			}}
-		/>
-
-		<button
-			class="btn text-lg"
-			on:click={() => {
-				selectedDay++;
-			}}>+</button
-		>
-	</div>
-
-	<button on:click={select} class="btn btn-primary" class:btn-disabled={selectedMonth == null}
-		>Add</button
-	>
+			No dates selected
+		</p>
+	{/if}
 </div>
 
 <style>
--- src/frontend/src/routes/learnatvcs/index.svelte
+++ src/frontend/src/routes/learnatvcs/index.svelte
@@ -1,1 +1,53 @@
-<a href="/learnatvcs/config" class="btn btn-primary">Start scraping</a>
+<script lang="ts">
+	import DateInput from '$lib/DateInput.svelte';
+
+	let dateType: 'latest' | 'custom' = 'latest';
+	let dateList: string[] = [];
+	$: if (dateType === 'custom') {
+		window.sessionStorage.setItem('config', JSON.stringify(dateList));
+	} else {
+		window.sessionStorage.removeItem('config');
+	}
+</script>
+
+<main class="ml-3">
+	<!-- XXX: should we center all of this? -->
+	<div class="mt-2 mx-auto w-3/4">
+		<h1 class="text-center text-4xl font-bold my-3">Scrape lesson plans</h1>
+		<p class="leading-relaxed text-xl">
+			This tool was literally build to scrape learn@vcs lesson plans for their assignments. Now,
+			choose the <b> date(s) of the lesson plan(s)</b> you want to download and extract dates from.
+		</p>
+	</div>
+	<div class="form-control max-w-sm my-3">
+		<label class="label cursor-pointer"
+			><span class="text-2xl">Latest dates</span>
+			<input
+				type="radio"
+				class="radio radio-lg radio-primary"
+				value="latest"
+				bind:group={dateType}
+				checked
+			/>
+		</label>
+		<label class="label cursor-pointer"
+			><span class="text-2xl">Custom dates</span>
+			<input
+				type="radio"
+				class="radio radio-lg radio-primary"
+				value="custom"
+				bind:group={dateType}
+			/>
+		</label>
+	</div>
+
+	{#if dateType == 'custom'}
+		<DateInput bind:selectedDates={dateList} />
+	{/if}
+	<!-- We're gonna leave some space for images -->
+	<a
+		class="my-5 btn btn-primary"
+		href="/learnatvcs/scrape"
+		class:btn-disabled={dateType == 'custom' && dateList.length == 0}>Alright, let's go</a
+	>
+</main>
