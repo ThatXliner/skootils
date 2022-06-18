874267319d768e13cca8ebc63b61e4501e652c86
Bryan Hu <bryan.hu.2020@gmail.com>
2022-06-18 14:40:44 -0700

    :sparkles: PowerSchool: implemented storage

--- src/powerschool/powerschool/__main__.py
+++ src/powerschool/powerschool/__main__.py
@@ -2,6 +2,15 @@ import json
 
 from powerschool import PowerSchool
 import keyring
+import time
+from pathlib import Path
+from platformdirs import user_data_dir
+
+APP_NAME = "Skootils"
+APP_AUTHOR = "Bryan Hu"
+HISTORY_DIR = Path(user_data_dir(APP_NAME, APP_AUTHOR)) / "powerschool" / "history"
+if not HISTORY_DIR.is_dir():
+    HISTORY_DIR.mkdir(parents=True)
 
 # Discuss: maybe put login details at argv?
 def main() -> None:
@@ -19,8 +28,18 @@ def main() -> None:
     with PowerSchool(
         "https://powerschool.vcs.net/public", auth["username"], auth["password"]
     ) as bot:
-        print(json.dumps(bot.get_quarters()))
-        print(json.dumps(bot.get(json.loads(input()))))
+        quarters = bot.get_quarters()
+        print(json.dumps(quarters))
+        output = json.dumps(bot.get(json.loads(input())))
+        print(output)
+        filename = f"{time.time()}.json"
+        for quarter in output:
+            key = quarters[-1] if quarter == "Latest" else quarter
+            (HISTORY_DIR / key).mkdir(exist_ok=True)
+
+            (HISTORY_DIR / key / filename).touch()
+            (HISTORY_DIR / key / filename).write_text(output[quarter])
+            (HISTORY_DIR / key / "info.json").write_text(json.dumps({"last": filename}))
 
 
 if __name__ == "__main__":
