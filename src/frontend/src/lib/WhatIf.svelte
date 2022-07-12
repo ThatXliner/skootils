71bc08247978f61ff9ecaf1951f8a1a78e9b8371
Bryan Hu <bryan.hu.2020@gmail.com>
2022-07-12 16:47:08 -0700

    :sparkles: Fully implemented

--- src/frontend/src/lib/WhatIf.svelte
+++ src/frontend/src/lib/WhatIf.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import AlertError from './AlertError.svelte';
+
 	export let currentScore: number;
 	export let assignmentCount: number;
 	let artificialAssignments: number[] = [];
@@ -21,36 +23,77 @@
 		(assignmentCount + artificialAssignments.length);
 </script>
 
-<p>What if I...</p>
+<!-- <div class="flex space-y-4 flex-col"> -->
+<!-- <p class="p-5 bg-amber-300 rounded-box">What if I...</p> -->
+<div class="flex justify-around">
+	<span class="p-3 bg-red-300 rounded-box">Current grade: <b>{currentGrade}%</b></span>
+	<span class="p-3 bg-blue-300 rounded-box">
+		<!-- 2 decimal points -->
+		Calculated grade: <b>{parseFloat(newScore.toPrecision(4))}%</b>
+	</span>
+</div>
 
-<p>Current grade: {currentGrade}</p>
-<p>Calculated grade: {newScore}</p>
-
-<ul class="mx-auto menu shadow-lg w-56 p-2 rounded-box max-h-md overflow-y-scroll">
-	{#each artificialAssignments as score, i}
-		<li>
-			<span
-				on:click={() => {
-					artificialAssignments.splice(i, 1);
-					artificialAssignments = artificialAssignments;
-				}}>{score}</span
-			>
-		</li>
+<div class="my-3">
+	{#if artificialAssignments.length > 0}
+		<div
+			class="bg-base-200 mx-auto shadow-lg p-2 rounded-box max-h-40 overflow-y-auto flex flex-wrap w-full"
+		>
+			{#each artificialAssignments as score, i}
+				<span
+					class="bg-info shadow-md p-2 m-1 rounded w-fit"
+					on:click={() => {
+						artificialAssignments.splice(i, 1);
+						artificialAssignments = artificialAssignments;
+					}}
+					>{score}%<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5 inline"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+							clip-rule="evenodd"
+						/>
+					</svg></span
+				>
+			{/each}
+		</div>
 	{:else}
-		<li><span>Please enter some assignments</span></li>
-	{/each}
-</ul>
-<h2>Add assignment</h2>
-<div>
-	<input type="number" min="0" bind:value={got} />/<input
-		type="number"
-		min="1"
-		bind:value={total}
-	/>
+		<AlertError message="Please enter some theoretical scores" extraClasses="mx-auto" />
+	{/if}
+</div>
+
+<div class="bg-base-300 rounded-box p-3 w-fit mx-auto">
+	<h2 class="text-lg font-bold mb-1">Add assignment</h2>
+	<input class="rounded bg-base-200 w-20 p-2 text-lg" type="number" min="0" bind:value={got} />
+	<span class="font-bold">out of</span>
+	<input class="rounded bg-base-200 w-20 p-2 text-lg" type="number" min="1" bind:value={total} />
+	<button
+		class="mx-2 float-right btn btn-primary"
+		on:click={() => {
+			artificialAssignments = [...artificialAssignments, (got / total) * 100];
+		}}>Add</button
+	>
 </div>
 <button
-	class="btn btn-primary"
+	class="mx-auto mt-3 btn btn-error"
 	on:click={() => {
-		artificialAssignments = [...artificialAssignments, (got / total) * 100];
-	}}>Add</button
+		artificialAssignments = [];
+	}}>Clear all</button
 >
+
+<!-- <div class="flex justify-center mx-auto">
+	<div class="bg-base-300 p-3 w-fit ">
+		<input class="rounded bg-base-200 w-20 p-3 text-lg" type="number" min="0" bind:value={got} />
+		<span class="font-bold">out of</span>
+		<input class="rounded bg-base-200 w-20 p-3 text-lg" type="number" min="1" bind:value={total} />
+	</div>
+	<button
+		class="btn btn-primary h-full"
+		on:click={() => {
+			artificialAssignments = [...artificialAssignments, (got / total) * 100];
+		}}>Add</button
+	>
+</div> -->
