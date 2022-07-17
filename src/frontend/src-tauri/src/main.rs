73490db1f7dd034374503be3981557b1f533fc3e
Bryan Hu <bryan.hu.2020@gmail.com>
2022-07-16 21:42:42 -0700

    We got the metadata in the back

--- src/frontend/src-tauri/src/main.rs
+++ src/frontend/src-tauri/src/main.rs
@@ -4,45 +4,12 @@
 )]
 #[macro_use]
 extern crate lazy_static;
-use tauri::{Menu, MenuItem, Submenu};
 mod commands;
 
 fn main() {
-    // TODO: Better menu
-    let menu = Menu::new()
-        .add_submenu(Submenu::new(
-            "app_name",
-            Menu::new()
-                // .add_native_item(MenuItem::About(
-                //     "app_name".to_string(),
-                //     AboutMetadata::new().copyright("All rights reserved".to_string()),
-                // ))
-                // .add_native_item(MenuItem::Separator)
-                // .add_native_item(MenuItem::Services)
-                // .add_native_item(MenuItem::Separator)
-                // .add_native_item(MenuItem::Hide)
-                // .add_native_item(MenuItem::HideOthers)
-                // .add_native_item(MenuItem::ShowAll)
-                // .add_native_item(MenuItem::Separator)
-                .add_native_item(MenuItem::Quit),
-        ))
-        .add_submenu(Submenu::new("Edit", {
-            let mut menu = Menu::new()
-                .add_native_item(MenuItem::Undo)
-                .add_native_item(MenuItem::Redo)
-                .add_native_item(MenuItem::Separator)
-                .add_native_item(MenuItem::Cut)
-                .add_native_item(MenuItem::Copy)
-                .add_native_item(MenuItem::Paste);
-            #[cfg(not(target_os = "macos"))]
-            {
-                menu = menu.add_native_item(MenuItem::Separator);
-            }
-            menu = menu.add_native_item(MenuItem::SelectAll);
-            menu
-        }));
+    let context = tauri::generate_context!();
     tauri::Builder::default()
-        .menu(menu)
+        .menu(tauri::Menu::os_default(&context.package_info().name))
         .invoke_handler(tauri::generate_handler![
             commands::get_teachers,
             commands::get_user_info,
@@ -50,6 +17,6 @@ fn main() {
             commands::delete,
             commands::data_dir_exists,
         ])
-        .run(tauri::generate_context!())
+        .run(context)
         .expect("error while running tauri application");
 }
