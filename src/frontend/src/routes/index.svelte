242f9369c359ee0985fc71c0f5f5f91f51bf0774
Bryan Hu <bryan.hu.2020@gmail.com>
2022-07-18 18:19:20 -0700

    Added credits

--- src/frontend/src/routes/index.svelte
+++ src/frontend/src/routes/index.svelte
@@ -58,36 +58,35 @@
 			</div>
 		</div>
 	</section>
-	<section class="h-screen bg-base-200" id="about">
-		<div class="hero h-[100%]">
-			<div class="hero-content flex-col">
-				<h1 class="text-5xl">About us</h1>
-				<p class="py-6 max-w-md">For now, this project is a one-man job.</p>
-				<b
-					>If you want to join, email me at <a
-						class="link link-primary"
-						href="mailto:bryan.hu@warriorlife.net">bryan.hu@warriorlife.net</a
-					></b
+	<footer class="footer p-10 bg-neutral text-neutral-content">
+		<div class="w-1/2">
+			<p>
+				Bryan Hu (@ThatXliner). <br /><br /> <b>Logo credits:</b> The logo's pencil is from
+				>Twitter's open source
+				<a href="https://twemoji.twitter.com" target="_blank" class="link">Twemoji emoji set</a>
+				(License: CC BY-SA 4.0). I have only rotated the pencil added the S.
+			</p>
+		</div>
+		<div class="w-3/4">
+			<span class="footer-title">Social</span>
+			<div class="grid grid-flow-col gap-4">
+				<a href="https://github.com/ThatXliner/skootils" target="_blank" title="Github Repository"
+					><svg
+						xmlns="http://www.w3.org/2000/svg"
+						aria-hidden="true"
+						role="img"
+						class="h-6 w-6 inline"
+						preserveAspectRatio="xMidYMid meet"
+						viewBox="0 0 24 24"
+						><path
+							fill="currentColor"
+							fill-rule="evenodd"
+							d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385c.6.105.825-.255.825-.57c0-.285-.015-1.23-.015-2.235c-3.015.555-3.795-.735-4.035-1.41c-.135-.345-.72-1.41-1.23-1.695c-.42-.225-1.02-.78-.015-.795c.945-.015 1.62.87 1.845 1.23c1.08 1.815 2.805 1.305 3.495.99c.105-.78.42-1.305.765-1.605c-2.67-.3-5.46-1.335-5.46-5.925c0-1.305.465-2.385 1.23-3.225c-.12-.3-.54-1.53.12-3.18c0 0 1.005-.315 3.3 1.23c.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23c.66 1.65.24 2.88.12 3.18c.765.84 1.23 1.905 1.23 3.225c0 4.605-2.805 5.625-5.475 5.925c.435.375.81 1.095.81 2.22c0 1.605-.015 2.895-.015 3.3c0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12Z"
+							clip-rule="evenodd"
+						/></svg
+					></a
 				>
-				<div>
-					<a href="https://github.com/ThatXliner/skootils" target="_blank" title="Github Repository"
-						><svg
-							xmlns="http://www.w3.org/2000/svg"
-							aria-hidden="true"
-							role="img"
-							class="h-6 w-6 inline"
-							preserveAspectRatio="xMidYMid meet"
-							viewBox="0 0 24 24"
-							><path
-								fill="currentColor"
-								fill-rule="evenodd"
-								d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385c.6.105.825-.255.825-.57c0-.285-.015-1.23-.015-2.235c-3.015.555-3.795-.735-4.035-1.41c-.135-.345-.72-1.41-1.23-1.695c-.42-.225-1.02-.78-.015-.795c.945-.015 1.62.87 1.845 1.23c1.08 1.815 2.805 1.305 3.495.99c.105-.78.42-1.305.765-1.605c-2.67-.3-5.46-1.335-5.46-5.925c0-1.305.465-2.385 1.23-3.225c-.12-.3-.54-1.53.12-3.18c0 0 1.005-.315 3.3 1.23c.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23c.66 1.65.24 2.88.12 3.18c.765.84 1.23 1.905 1.23 3.225c0 4.605-2.805 5.625-5.475 5.925c.435.375.81 1.095.81 2.22c0 1.605-.015 2.895-.015 3.3c0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12Z"
-								clip-rule="evenodd"
-							/></svg
-						></a
-					>
-				</div>
 			</div>
 		</div>
-	</section>
+	</footer>
 </main>
