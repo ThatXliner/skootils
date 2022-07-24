401d19be1eb567f31e46a5cda6c4b1f3969b8196
Bryan Hu <bryan.hu.2020@gmail.com>
2022-07-24 16:35:45 -0700

    :alembic: Better date highlighting

--- src/learnatvcs/learnatvcs/highlighter.py
+++ src/learnatvcs/learnatvcs/highlighter.py
@@ -7,6 +7,9 @@ from bs4 import BeautifulSoup
 from learnatvcs.process import to_soup
 
 # TODO: Re-organize
+_SHORT_DATE = r"(?:\d+)/(?:\d+)"
+_START_KEYWORDS = r"(?:on|(?:start\w+)|(?:begin\w+)|(?:next\w+))"
+_DUAL_DATE = rf"(?:{_SHORT_DATE}(?:\s*&\s*{_SHORT_DATE})?)"
 MONTHS = [
     "january",
     "february",
@@ -21,33 +24,54 @@ MONTHS = [
     "november",
     "december",
 ]
-times = [
-    "monday",
-    "tuesday",
-    "wednesday",
-    "thursday",
-    "friday",
-    "today",
-    "tomorrow",
-]
-periods = (
-    [
-        "((next|following|in) )?class",
-        "in-class",
-        "((next|following) )?period",
-        "((next|following) )?week",
-        "((next|following) )?month",
-        "((next|following) )?year",
+times = (
+    [rf"(?:{month}(?: \d+\w*)?)" for month in MONTHS]
+    + MONTHS
+    + [
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "today",
+        "tomorrow",
+        "week",
+        "month",
+        "year",
+        "period",
+        "class",
     ]
-    + [rf"{month}(?: \d+\w*)?" for month in MONTHS]
-    + [rf"((next|following) )?{time}" for time in times]
 )
-DAYS = f'(?:{"|".join(periods)})'
+_RELATIVE_KEYWORDS = "|".join(
+    f"(?:{pat})"
+    for pat in {"next", "the following", "following", "in", "during", "at", "when"}
+)
+_WORD_DATE = f"(?:{'|'.join(times)})"
+DATE = (
+    rf"(?:(?:{_START_KEYWORDS}|{_RELATIVE_KEYWORDS})?\s*(?:{_DUAL_DATE}|{_WORD_DATE}))"
+)
+# TIMES = ""
+#
+# periods = (
+#     [
+#         "((next|following|in) )?class",
+#         "in-class",
+#         "((next|following) )?period",
+#         "((next|following) )?week",
+#         "((next|following) )?month",
+#         "((next|following) )?year",
+#     ]
+#     + [rf"{month}(?: \d+\w*)?" for month in MONTHS]
+#     + [rf"((next|following) )?{time}" for time in times]
+# )
+# DAYS = f'(?:{"|".join(periods)})'
 wordlist = [
-    "(next|following) (class|period|week|month|year)",
-    r"(begin|continue|finish) \w+ing",
-    f"due(.+{DAYS})",
-    rf"by(.+{DAYS})",
+    rf"(due|by)(.+{DATE})",
+    r"(begin|continue|finish)( \w+ing)?",
+    # "(next|following) (class|period|week|month|year)",
+    # r"(begin|continue|finish) \w+ing",
+    # f"due(.+{DAYS})",
+    # rf"by(.+{DAYS})",
 ]
 
 HIGHLIGHT_ME = re.compile(
