b2f7b24e1584b626eedbb2a29ec4f7b6a8cbb983
Bryan Hu <bryan.hu.2020@gmail.com>
2022-08-21 09:10:05 -0700

    :sparkles: Added --full to rebuild script

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -2,7 +2,10 @@
 # This script is mainly for developers.
 # It only works on a UNIX-like environment
 rm -rf src/learnatvcs/dist src/powerschool/dist
-
+if [ "$1" = "--full" ]
+then
+    rm -rf src/learnatvcs/build src/powerschool/build
+fi
 cd src/learnatvcs
 echo *.spec | xargs -n 1 pyinstaller &
 cd ../powerschool
