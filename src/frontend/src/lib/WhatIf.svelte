a4e943adcfbe247d4250a94f73651b6cd9e38058
Bryan Hu <bryan.hu.2020@gmail.com>
2022-10-22 22:03:12 -0700

    ok recovered changed
    git reflog too op

--- src/frontend/src/lib/WhatIf.svelte
+++ src/frontend/src/lib/WhatIf.svelte
@@ -47,11 +47,9 @@
 		<span class="p-3 bg-red-300 rounded-box">Current grade: <b>{currentScore}%</b></span>
 		<span class="p-3 bg-blue-300 rounded-box">
 			<!-- 2 decimal points -->
-			Calculated grade:
-			<b>{parseFloat(newScore.toPrecision(4))}%</b>
+			Calculated grade: <b>{parseFloat(newScore.toPrecision(4))}%</b>
 		</span>
 	</div>
-
 	<WeightPicker {categories} bind:weights />
 	{#if artificialAssignments.length > 0}
 		<div
@@ -76,32 +74,46 @@
 						/>
 					</svg></span
 				>
-				<path
-					fill-rule="evenodd"
-					d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-					clip-rule="evenodd"
-				/>
 			{/each}
 		</div>
 	{:else}
 		<AlertError message="Please enter some theoretical scores" extraClasses="mx-auto" />
 	{/if}
-</div>
 
-<div class="bg-base-300 rounded-box p-3 w-fit mx-auto">
-	<h2 class="text-lg font-bold mb-1">Add assignment</h2>
-	<input class="rounded bg-base-200 w-20 p-2 text-lg" type="number" min="0" bind:value={got} />
-	<span class="font-bold">out of</span>
-	<input class="rounded bg-base-200 w-20 p-2 text-lg" type="number" min="1" bind:value={total} />
-	<button
-		class="mx-2 float-right btn btn-primary"
-		on:click={() => {
-			artificialAssignments = [...artificialAssignments, (got / total) * 100];
-		}}>Add</button
-	>
+	<div class="bg-base-300 rounded-box p-3 max-w-fit mx-auto">
+		<div>
+			<h2 class="text-lg font-bold mb-1">Add assignment</h2>
+			<input class="rounded bg-base-200 w-20 p-2 text-lg" type="number" min="0" bind:value={got} />
+			<span class="font-bold">out of</span>
+			<input
+				class="rounded bg-base-200 w-20 p-2 text-lg"
+				type="number"
+				min="1"
+				bind:value={total}
+			/>
+			<button
+				class="mx-2 float-right btn btn-primary"
+				disabled={selectedCategory == 'Pick one'}
+				on:click={() => {
+					artificialAssignments = [
+						...artificialAssignments,
+						[selectedCategory, (got / total) * 100]
+					];
+					selectedCategory = 'Pick one';
+				}}>Add</button
+			>
+		</div>
+		<select class="select select-bordered w-full mt-3" bind:value={selectedCategory}>
+			<option disabled selected>Pick one</option>
+			{#each categories as category}
+				<option>{category}</option>
+			{/each}
+		</select>
+	</div>
 </div>
 <button
 	class="mx-auto mt-3 btn btn-error"
+	disabled={artificialAssignments.length == 0}
 	on:click={() => {
 		artificialAssignments = [];
 	}}>Clear all</button
