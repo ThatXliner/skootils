0ecd67f5079ab92416e389ad47161619eaef5432
Bryan Hu <bryan.hu.2020@gmail.com>
2022-11-21 09:27:55 -0800

    :poop: Multithreading?

--- src/learnatvcs.rs/src/lib.rs
+++ src/learnatvcs.rs/src/lib.rs
@@ -1,13 +1,14 @@
 // TODO: optimize
 #![warn(missing_docs)]
 //! Fast request-based scraping API for learn@vcs
-
+use std::pin::Pin;
 /// Date models
 pub mod datematcher;
 /// Error types
 pub mod errors;
 use datematcher::{ClassDay, Date};
 /// Choose a date to scrape
+#[derive(Clone)]
 pub enum TargetDate {
     /// Scrape latest dates
     Latest,
@@ -27,6 +28,9 @@ use reqwest;
 use scraper::{Html, Selector};
 use std::collections::HashMap;
 use std::io::{self, Write};
+use tokio;
+use tokio::sync::mpsc;
+use tokio::task::spawn_blocking;
 /// Closely follows learn@vcs's structure
 pub type Output = HashMap<String, Option<HashMap<String, Option<String>>>>;
 lazy_static! {
@@ -129,7 +133,12 @@ impl Session {
     }
 
     /// TODO: Scrape spec and multiple quarters and dates
-    pub async fn scrape(&self, quarter: Option<usize>, dates: TargetDate) -> Result<Output> {
+    pub async fn scrape(
+        // self: Pin<&Self>,
+        &self,
+        quarter: Option<usize>,
+        dates: TargetDate,
+    ) -> Result<Output> {
         let dom = Html::parse_document(self.cached_homepage.as_str());
 
         let mut tasks = Vec::new();
@@ -144,17 +153,33 @@ impl Session {
             if name.find("-") == None {
                 break;
             }
-            let url = class_link.value().attr("href").unwrap();
-            let dates_ref = &dates;
+
+            let url = class_link.value().attr("href").unwrap().to_owned();
             eprintln!("Class: {}, url: {}", name, url);
+            let dates_clone = dates.clone();
+
             // we do minimal parsing here
-            tasks.push(async move { (name, self.scrape_page(url, quarter, dates_ref).await.ok()) });
+            // tasks.push(tokio::spawn(async move {
+            //     (
+            //         String::from(""),
+            //         self.scrape_page("url", None, &TargetDate::All).await,
+            //     )
+            // }));
+            tasks.push(tokio::spawn(async move {
+                (
+                    name,
+                    self.scrape_page(&url, quarter, &dates_clone).await.ok(),
+                )
+            }));
         }
         eprintln!("LEVEL 1: Root scrape loop");
         // Concurrent; not parallel. May make it parallel in the future if it's even faster
-        for (key, value) in join_all(tasks).await {
-            output.insert(key, value);
+        for res in join_all(tasks).await {
+            if let Ok((key, value)) = res {
+                output.insert(key, value);
+            }
         }
+        // Err(LearnAtVcsError::InvalidQuarter)
         Ok(output)
     }
     /// Given URL of learn@vcs page, find lesson plans
@@ -164,91 +189,59 @@ impl Session {
         quarter: Option<usize>,
         dates: &TargetDate,
     ) -> Result<HashMap<String, Option<String>>> {
-        let page = Html::parse_document(self.get_page(url).await?.as_str());
-        let link = page
-            .select(&LESSON_PLAN_LINK_SELECTOR)
-            .next()
-            .map(|element| element.value().attr("href").unwrap())
-            .ok_or(LearnAtVcsError::NoLessonPlans)?; // shouldn't happen
-        let plan_quarters = Html::parse_document(self.get_page(link).await?.as_str());
-        let quarter_element = match quarter {
-            None => plan_quarters.select(&LESSON_PLAN_QUARTER_SELECTOR).last(),
-            Some(quarter_num) => 'output: {
-                // XXX: This is probably highly inefficient
+        let contents = self.get_page(url).await?;
+        let link = {
+            Html::parse_document(&contents)
+                .select(&LESSON_PLAN_LINK_SELECTOR)
+                .next()
+                .map(|element| element.value().attr("href").unwrap())
+                .ok_or(LearnAtVcsError::NoLessonPlans)? // shouldn't happen
+                .to_owned()
+        };
+        let learnatvcs_page_contents = self.get_page(&link).await?;
+        let quarter_url = {
+            let plan_quarters = Html::parse_document(&learnatvcs_page_contents);
+            let quarter_element = match quarter {
+                None => plan_quarters.select(&LESSON_PLAN_QUARTER_SELECTOR).last(),
+                Some(quarter_num) => 'output: {
+                    // XXX: This is probably highly inefficient
 
-                // For every link we have, find the link with the quarter number in its name
-                for element in plan_quarters.select(&LESSON_PLAN_QUARTER_SELECTOR) {
-                    let Some(text_element) =
+                    // For every link we have, find the link with the quarter number in its name
+                    for element in plan_quarters.select(&LESSON_PLAN_QUARTER_SELECTOR) {
+                        let Some(text_element) =
                         element.select(&LESSON_PLAN_QUARTER_TEXT_SELECTOR).next() else {continue};
-                    let Some(text_node) = text_element.text().next() else {continue};
+                        let Some(text_node) = text_element.text().next() else {continue};
 
-                    if text_node.contains(&quarter_num.to_string()) {
-                        eprintln!("Quarter found");
-                        break 'output Some(element);
+                        if text_node.contains(&quarter_num.to_string()) {
+                            eprintln!("Quarter found");
+                            break 'output Some(element);
+                        }
                     }
+                    None
                 }
-                None
             }
-        }
-        .ok_or(LearnAtVcsError::InvalidQuarter)?;
-        Ok(self
-            .scrape_plans(quarter_element.value().attr("href").unwrap(), &dates)
-            .await?)
+            .ok_or(LearnAtVcsError::InvalidQuarter)?;
+            quarter_element.value().attr("href").unwrap().to_owned()
+        };
+        Ok(self.scrape_plans(&quarter_url, &dates).await?)
     }
     async fn scrape_plans(
         &self,
         url: &str,
         dates: &TargetDate,
     ) -> Result<HashMap<String, Option<String>>> {
-        let lesson_plans = Html::parse_document(self.get_page(url).await?.as_str());
-        let mut tasks = Vec::new();
-        let links = lesson_plans.select(&LESSON_PLAN_DATE_SELECTOR);
-        let mut add_task = |date_name: String, plan_url: String| {
-            tasks.push(async move { (date_name, self.scrape_plan(plan_url.as_str()).await.ok()) });
-        };
-        match dates {
-            TargetDate::Latest => {
-                let res = links.last().ok_or(LearnAtVcsError::StructureChanged)?;
-                let date_name = res
-                    .value()
-                    .attr("title")
-                    .or_else(|| res.text().next())
-                    .ok_or(LearnAtVcsError::StructureChanged)?;
-                let plan_url = res
-                    .value()
-                    .attr("href")
-                    .map(|link| format!("{}/mod/book/{}", BASE_URL, link))
-                    .or_else(|| Some(url.to_string()))
-                    .unwrap();
-                eprintln!("Scraping for {date_name} (url: {plan_url})");
-                add_task(date_name.to_string(), plan_url);
-            }
-            remaining => {
-                // For every date, match the text with the given date
-                for res in links.filter(|element| match remaining {
-                    TargetDate::Selected(dates) => {
-                        let class_day = ClassDay::from_str(
-                            element
-                                .value()
-                                .attr("title")
-                                .or_else(|| element.text().next())
-                                .unwrap(),
-                        )
-                        .unwrap();
-                        for date in dates {
-                            if class_day.matches(&date) {
-                                return true;
-                            }
-                        }
-                        return false;
-                    }
-                    TargetDate::All => {
-                        return true;
-                    }
-                    _ => {
-                        unreachable!()
-                    }
-                }) {
+        // TODO: Remove unnecessary requests by reusing `lesson_plans` via Box::pin(future) and Box(dyn future)
+        let tasks = {
+            let lesson_plans = Html::parse_document(&self.get_page(url).await?);
+            let mut tasks = Vec::new();
+            let links = lesson_plans.select(&LESSON_PLAN_DATE_SELECTOR);
+
+            let mut add_task = |date_name: String, plan_url: String| {
+                tasks.push(async move { (date_name, self.scrape_plan(&plan_url).await.ok()) });
+            };
+            match dates {
+                TargetDate::Latest => {
+                    let res = links.last().ok_or(LearnAtVcsError::StructureChanged)?;
                     let date_name = res
                         .value()
                         .attr("title")
@@ -260,22 +253,66 @@ impl Session {
                         .map(|link| format!("{}/mod/book/{}", BASE_URL, link))
                         .or_else(|| Some(url.to_string()))
                         .unwrap();
-                    // eprintln!("Scraping for {date_name} (url: {plan_url})");
+                    eprintln!("Scraping for {date_name} (url: {plan_url})");
                     add_task(date_name.to_string(), plan_url);
                 }
-            }
-        }
+                remaining => {
+                    // For every date, match the text with the given date
+                    for res in links.filter(|element| match remaining {
+                        TargetDate::Selected(dates) => {
+                            let class_day = ClassDay::from_str(
+                                element
+                                    .value()
+                                    .attr("title")
+                                    .or_else(|| element.text().next())
+                                    .unwrap(),
+                            )
+                            .unwrap();
+                            for date in dates {
+                                if class_day.matches(&date) {
+                                    return true;
+                                }
+                            }
+                            return false;
+                        }
+                        TargetDate::All => {
+                            return true;
+                        }
+                        _ => {
+                            unreachable!()
+                        }
+                    }) {
+                        let date_name = res
+                            .value()
+                            .attr("title")
+                            .or_else(|| res.text().next())
+                            .ok_or(LearnAtVcsError::StructureChanged)?;
+                        let plan_url = res
+                            .value()
+                            .attr("href")
+                            .map(|link| format!("{}/mod/book/{}", BASE_URL, link))
+                            .or_else(|| Some(url.to_string()))
+                            .unwrap();
+                        // eprintln!("Scraping for {date_name} (url: {plan_url})");
+                        add_task(date_name.to_string(), plan_url);
+                    }
+                }
+            };
+            tasks
+        };
         eprintln!("LEVEL 2: Per-date scrape loop");
         let mut output = HashMap::new();
         for (key, value) in join_all(tasks).await {
+            // if let Ok((key, value)) = res {
             output.insert(key, value);
+            // }
         }
         Ok(output)
     }
 
     async fn scrape_plan(&self, url: &str) -> Result<String> {
         // eprintln!("Parsing lesson plans for {url}");
-        Html::parse_document(self.get_page(url).await?.as_str())
+        Html::parse_document(&self.get_page(url).await?)
             .select(&LESSON_PLAN_CONTENTS_SELECTOR)
             .next()
             .map(|element| element.inner_html())
