ded6045b390d90de22419df0288d03c14c3068aa
Bryan Hu <bryan.hu.2020@gmail.com>
2022-11-23 23:03:41 -0800

    Logs to stderr

--- src/learnatvcs.rs/src/main.rs
+++ src/learnatvcs.rs/src/main.rs
@@ -1,6 +1,7 @@
 use learnatvcs::scrape;
 use serde_json;
 use std::env;
+use std::io;
 use tokio;
 use tracing_subscriber;
 static LEARNATVCS_USERNAME: &str = "LEARNATVCS_USERNAME";
@@ -9,13 +10,15 @@ static LEARNATVCS_PASSWORD: &str = "LEARNATVCS_PASSWORD";
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let subscriber = tracing_subscriber::FmtSubscriber::new();
     let subscriber = tracing_subscriber::fmt()
-        .pretty()
+        // .pretty()
         // Display source code file paths
         .with_file(true)
         // Display source code line numbers
         .with_line_number(true)
         // Display the thread ID an event was recorded on
         .with_thread_ids(true)
+        // Send to standard error
+        .with_writer(io::stderr)
         .finish();
     // use that subscriber to process traces emitted after this point
     tracing::subscriber::set_global_default(subscriber)?;
