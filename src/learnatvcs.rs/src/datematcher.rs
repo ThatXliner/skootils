6a2779b212c7b0e9092a462e125ac89d465eed2b
Bryan Hu <bryan.hu.2020@gmail.com>
2023-02-22 16:07:53 -0800

    Datematcher final fixes

--- src/learnatvcs.rs/src/datematcher.rs
+++ src/learnatvcs.rs/src/datematcher.rs
@@ -10,12 +10,10 @@ lazy_static! {
     /// 3. `amonth aday/bday`
     /// 4. `amonth aday/bmonth bday`
     /// 5. `amonthaday` (e.g. `WK#1 Jan4-6 (Zoom Jan5)` gets `Jan 4-6` and `Jan5`)
-    ///
-    /// The regex can be found at https://regex101.com/r/HjMkar/2
-    ///
-    /// TODO: Support `Unit 2: Continued January 4/5`. Think of iterating through all captures
+    /// 6. `amonth aday - bmonth bday`
+    /// The regex can be found at https://regex101.com/r/HjMkar/3
     static ref CLASS_DAY_RE: Regex = Regex::new(
-        r"(?:(?P<amonth>[a-zA-Z0-9]+)[. ](?P<aday>\d+))(?:(?:\s*(?:/|-| \-))\s*((?P<bmonthorday>(?:[a-zA-Z]+|[0-9]+))(?:[. ](?P<bday>\d+))?)?)?"
+        r"(?:(?P<amonth>[a-zA-Z0-9]+)[. ]?(?P<aday>\d+))(?:(?:\s*(?:/|-| \-))\s*((?P<bmonthorday>(?:[a-zA-Z]+|[0-9]+))(?:[. ](?P<bday>\d+))?)?)?"
     )
     .unwrap();
     static ref DATE_RE: Regex = Regex::new(r"(?P<month>[a-zA-Z]+) (?P<day>\d+)").unwrap();
@@ -43,7 +41,7 @@ fn validate(month: u8, day: u8) -> Result<(), DateError> {
 fn normalize_month(month: &str) -> Option<u8> {
     // Try numeric month first
     month.parse::<u8>().ok().or_else(|| {
-    month
+        month
             .get(0..3)
             .and_then(|month| MONTH2INT.get(&month.to_lowercase()).copied())
     })
@@ -121,55 +119,66 @@ impl PartialEq for ClassDay {
 impl FromStr for ClassDay {
     type Err = DateError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let caps = CLASS_DAY_RE.captures(s).ok_or(DateError::ParseError)?;
-        let a_month =
-            normalize_month(caps.name("amonth").unwrap().as_str()).ok_or(DateError::ParseError)?;
-        let a_day = caps
-            .name("aday")
-            .unwrap()
-            .as_str()
-            .parse::<u8>()
-            .or(Err(DateError::ParseError))?;
-        let b_day = match caps.name("bmonthorday") {
-            Some(b_month) => {
-                match caps.name("bday") {
-                    Some(b_day) => {
-                        if let Some(b_month) = normalize_month(b_month.as_str()) {
-                            Some(
-                                Date::new(
-                                    b_month,
-                                    b_day
-                                        .as_str()
-                                        .parse::<u8>()
-                                        .map_err(|_| DateError::ParseError)?,
-                                )
-                                .expect("Invalid date"),
-                            )
-                        } else {
-                            // Examples: January 4 - Intro, February 7 - Unit 2, etc
-                            None
-                        }
-                    }
-                    None => {
-                        // It's actually a day
-                        b_month
-                            .as_str()
-                            .parse::<u8>()
-                            // Examples of cases that return None: January 4 - U7
-                            .ok()
-                            .map(|b_day| Date::new(a_month, b_day).expect("Invalid date"))
-                    }
-                }
+        let parsed = CLASS_DAY_RE
+            .captures_iter(s)
+            .filter_map(|caps| parse_date_capture(caps).ok())
+            .collect::<Vec<ClassDay>>();
+        match parsed.len() {
+            2 => {
+                // 2 days... one of the is A day and one is actually B day
+                Ok(ClassDay {
+                    a: parsed[0].a,
+                    b: Some(parsed[1].a),
+                })
             }
-            None => {
-                // Just one day
-                // It doesn't actually matter if it happens to be an actual A-day or a B-day
-                None
-            }
-        };
-        Ok(ClassDay {
-            a: Date::new(a_month, a_day).unwrap(),
-            b: b_day,
-        })
+            1 => Ok(parsed[0]),
+            _ => Err(DateError::ParseError),
+        }
     }
+}
+
+fn parse_date_capture(caps: regex::Captures) -> Result<ClassDay, DateError> {
+    let a_month =
+        normalize_month(caps.name("amonth").unwrap().as_str()).ok_or(DateError::ParseError)?;
+    let a_day = caps
+        .name("aday")
+        .unwrap()
+        .as_str()
+        .parse::<u8>()
+        .or(Err(DateError::ParseError))?;
+    let b_day = caps.name("bmonthorday").and_then(|b_month| {
+        caps.name("bday")
+            // not using .map_or_else since this is more clear
+            .map(|b_day| bmonth_and_bday(b_month, b_day))
+            .unwrap_or_else(|| a_month_and_aday(a_month, b_month))
+    });
+    Ok(ClassDay {
+        a: Date::new(a_month, a_day).unwrap(),
+        b: b_day,
+    })
+}
+fn a_month_and_aday(a_month: u8, b_month: regex::Match) -> Option<Date> {
+    // It's actually a day
+    b_month
+        .as_str()
+        .parse::<u8>()
+        // Examples of cases that return None: January 4 - U7
+        .ok()
+        .map(|b_day| Date::new(a_month, b_day).expect("Invalid date"))
+}
+fn bmonth_and_bday(b_month: regex::Match, b_day: regex::Match) -> Option<Date> {
+    normalize_month(b_month.as_str()).and_then(|b_month| {
+        Some(
+            Date::new(
+                b_month,
+                // XXX: Don't fail silently?
+                match b_day.as_str().parse::<u8>() {
+                    Ok(it) => it,
+                    Err(_) => return None,
+                },
+            )
+            .expect("Invalid date"),
+        )
+    })
+    // Examples of None: January 4 - Intro, February 7 - Unit 2, etc
 }
